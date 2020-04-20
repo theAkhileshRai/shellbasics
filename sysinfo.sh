bbb2295f5e6f5351df0fcf92493c06bc184fe15d
Akhilesh Rai <arsrocz@gmail.com>
2020-04-20 12:48:31 +0530

    Create sysinfo.sh

---
+++ sysinfo.sh
@@ -0,0 +1,19 @@
+# running
+
+# Tell the user hostname
+
+hostname
+
+# Display the current date and time when this info was collected
+
+date
+
+# Display kernel release followed by architecture
+
+uname -r
+uname -m
+
+# Display the disk usage in readable format
+
+df -h
+
